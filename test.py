f3658aec6fc952661786f3f537ea27ea7c7c8651
eileen0904 <xinlingzhang04@gmail.com>
2023-05-06 14:16:28 +0800

    for_rep

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-import numpy as np
-import os
 import cv2
-import pathlib
-import math
 import mediapipe as mp
-from rembg import remove
 
 color_green = (0, 255, 0)
 color_red = (0, 0, 255)
@@ -12,13 +7,15 @@ color_blue = (255, 0, 0)
 low_bound = 30
 upper_bound = 200
 
-img = cv2.imread('test_tran.jpg')
+img = cv2.imread('1234.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 canny_img = cv2.Canny(gray, low_bound, upper_bound)
 circles = cv2.HoughCircles(canny_img, cv2.HOUGH_GRADIENT, 1,
-                           20, param1=120, param2=30, minRadius=5, maxRadius=50)
+                               20, param1=120, param2=30, minRadius=5, maxRadius=50)
 
+mp_drawing = mp.solutions.drawing_utils          # mediapipe 繪圖方法
+mp_drawing_styles = mp.solutions.drawing_styles  # mediapipe 繪圖樣式
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=1)
 result = hands.process(img)  # 偵測手
@@ -31,6 +28,12 @@ if result.multi_hand_landmarks:
     wrist_x = 0  # 腕關節
     wrist_y = 0
     for handLms in result.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+                    img,
+                    handLms,
+                    hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
         for i, lm in enumerate(handLms.landmark):
             xPos = round(lm.x * W)
             yPos = round(lm.y * H)
@@ -39,17 +42,14 @@ if result.multi_hand_landmarks:
                 wrist_y = yPos
     wrist_y = wrist_y + 15
     for circle in circles[0]:
-        # 座標行列
         x = int(circle[0])
         y = int(circle[1])
         r = int(circle[2])  # 半徑
         rel_x = abs(x - wrist_x)
         rel_y = abs(y - wrist_y)
-        #print("x:", rel_x, "y:", rel_y)
         img = cv2.circle(img, (x, y), r, color_red, 1)
         img = cv2.circle(img, (x, y), 2, color_blue, 1)
-        coor.append([rel_x, rel_y])
-        radius.append(r)
+    #cv2.imwrite("for_rep", img)
     cv2.imshow("new", img)
 
 cv2.waitKey(0)
